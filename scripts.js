37d7eec2934a860fb5a072664c963469c458ea98
Jaswin <winjas2020@gmail.com>
2024-12-05 21:53:53 +0530

    coded the html file buttons and event listeners in script : day1

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const add=document.getElementById('addbtn')
+const minus=document.getElementById('minusbtn')
+const cross=document.getElementById('crossbtn')
+const div=document.getElementById('divbtn')
+const mod=document.getElementById('modbtn')
+const a
+const b
+
+addEventListener('click', ()=>{
+    return a + b
+})
